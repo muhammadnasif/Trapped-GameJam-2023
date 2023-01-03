e95a5914156e50a6e9c1354b7ba01dde4580241a
Mehedi Hasan <57999057+Moidda@users.noreply.github.com>
2023-01-03 22:10:47 +0600

    Walls, movement and jump

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody2D rb;
-    [SerializeField] int jumpForce;
+    BoxCollider2D coll;
 
-    void Start()
-    {
+    [SerializeField] int jumpForce;
+    [SerializeField] int moveSpeed;
+
+    [SerializeField] LayerMask jumpableGround;
+
+
+    void Start() {
         jumpForce = 12;
+        moveSpeed = 7;
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
-    void Update()
-    {
-        if(Input.GetKeyDown("space")) {
+    private void Update() {
+        moveHorizontal();
+        jump();
+    }
+
+    private void moveHorizontal() {
+        float dirX = moveSpeed * Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+
+    private void jump() {
+        if(Input.GetButtonDown("Jump") && isGrounded()) {
             rb.velocity = new Vector3(0, jumpForce, 0);
         }
+    }
+
+    private bool isGrounded() {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .01f, jumpableGround);
     }
 }
